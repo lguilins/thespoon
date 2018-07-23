2442d6f2c62e3f312070f7500419ab13cfb64b5f
Luiz Guilherme Lins <lins.luizguilherme@gmail.com>
2018-07-23 18:07:46 -0300

    implement a simple Rails application with two pages

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get contact" do
+    get pages_contact_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
+
+  test "should get question" do
+    get pages_question_url
+    assert_response :success
+  end
+
+end
